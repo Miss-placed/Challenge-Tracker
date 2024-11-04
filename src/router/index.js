9420e29ab6e68949b5b68c1d6ac6c65d4052776d
sol3uk <ben@sol3.me>
2024-11-04 16:42:16 +0000

    Add form route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +28,11 @@ const router = createRouter({
       path: '/:game/:gamemode/:category/:subcategory',
       name: 'subcategoryView',
       component: () => import('@/views/SubcategoryView.vue'),
+    },
+    {
+      path: '/form',
+      name: 'formView',
+      component: () => import('@/views/FormView.vue'),
     },
   ],
 })
